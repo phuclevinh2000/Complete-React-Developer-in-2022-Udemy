5da3ba38cea357ba4b3e159916cb0241fb781a33
Phuc Le <phuclevinh2000@gmail.com>
2022-07-19 23:52:43 +0300

    update to functional component

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,8 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect, Component } from 'react';
 
 import './App.css';
 import CardList from './components/CardList/CardList';
 import SearchBox from './components/SearchBox/SearchBox';
+
+const App = () => {
+  const [monsters, setMonsters] = useState();
+  const [searchField, setSearchField] = useState();
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  return (
+    <div className='App'>
+      <h1 className='app-title'>Monsters Rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder='search monsters'
+        className='monsters-search-box'
+      />
+
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default class App extends Component {
   constructor() {
