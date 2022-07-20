42d2ba0fd1ba36455c77f2375d7d97cfadbc33bc
Phuc Le <phuclevinh2000@gmail.com>
2022-07-21 00:48:10 +0300

    destructure component

--- monster-rolodex/src/components/CardList/CardList.jsx
+++ monster-rolodex/src/components/CardList/CardList.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Card from '../Card/Card';
 import './CardList.css';
 
-const CardList = (props) => {
-  const { monsters } = props;
+const CardList = ({ monsters }) => {
   return (
     <div className='card-list'>
       {monsters.map((monster) => {
