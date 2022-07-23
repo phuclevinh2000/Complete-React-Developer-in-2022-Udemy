475792c70caf6d72e698587314ca01d67aa4f699
Phuc Le <phuclevinh2000@gmail.com>
2022-07-24 00:21:24 +0300

    added sign in component and firebase

--- crwn-clothing/src/utils/firebase/firebase.js
+++ crwn-clothing/src/utils/firebase/firebase.js
@@ -42,5 +42,17 @@ export const createUserDocumentFromAuth = async (userAuth) => {
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
     const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
   }
+
+  return userDocRef;
 };
